1c21cf8d7fb1231674aa23e2113385f48d594423
Hau Nguyen <hauvipapro@gmail.com>
2021-06-23 17:43:55 +0000

    refactor: move resolution to single file

--- dpi.go
+++ dpi.go
@@ -1,24 +1,5 @@
 package dpi
 
-import "math"
-
-// Unit is pixel
-type Resolution struct {
-	width  int64
-	height int64
-}
-
-func NewResolution(width, height int64) Resolution {
-	return Resolution{
-		width:  width,
-		height: height,
-	}
-}
-
-func (r Resolution) CalculateDiagonal() float64 {
-	return math.Sqrt(float64(r.width*r.width + r.height*r.height))
-}
-
 // Unit diagonal is inch
 func CalculateDPI(r Resolution, diagonal float64) float64 {
 	if diagonal == 0 {
